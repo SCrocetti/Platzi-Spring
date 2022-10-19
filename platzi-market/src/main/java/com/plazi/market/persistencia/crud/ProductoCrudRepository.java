94565dab32c93d91ac37a2fac5c8315138309417
SofiaExpirience <sofia.expirience@gmail.com>
2022-10-19 20:57:26 -0300

    Añadir algunos Query Methods como ejemplos.

--- platzi-market/src/main/java/com/plazi/market/persistencia/crud/ProductoCrudRepository.java
+++ platzi-market/src/main/java/com/plazi/market/persistencia/crud/ProductoCrudRepository.java
@@ -3,5 +3,13 @@ package com.plazi.market.persistencia.crud;
 import com.plazi.market.persistencia.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+    // query method.
+    List<Producto> findByIdCategoriaOrderByNombreDesc(int idCategoria);
+
+    // basicamente podes hablarle en lenguaje natural XD
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
 }

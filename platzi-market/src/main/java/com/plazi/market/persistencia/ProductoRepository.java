0799ac90fe036d3c49388f70654cdf97d256d976
SofiaExpirience <sofia.expirience@gmail.com>
2022-10-19 21:18:12 -0300

    Implementar @Repository

--- platzi-market/src/main/java/com/plazi/market/persistencia/ProductoRepository.java
+++ platzi-market/src/main/java/com/plazi/market/persistencia/ProductoRepository.java
@@ -2,10 +2,11 @@ package com.plazi.market.persistencia;
 
 import com.plazi.market.persistencia.crud.ProductoCrudRepository;
 import com.plazi.market.persistencia.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll(){
@@ -15,4 +16,10 @@ public class ProductoRepository {
     public List<Producto> getByCategoria(int idCategoria){return productoCrudRepository.findByIdCategoriaOrderByNombreDesc(idCategoria);}
 
     public Optional<List<Producto>> getEscasos(int cantidad){return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);}
+
+    public Optional<Producto> getProducto(int idProducto){return productoCrudRepository.findById(idProducto);}
+
+    public Producto save(Producto producto){return productoCrudRepository.save(producto);}
+
+    public  void  delete(int idProducto){productoCrudRepository.deleteById(idProducto);}
 }

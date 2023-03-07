154057520a0600c4280307836b5a067e69709e43
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2023-03-07 12:46:13 -0300

    FEAT add docker-compose to the project

--- platzi-market/src/main/java/com/plazi/market/web/security/SecurityConfig.java
+++ platzi-market/src/main/java/com/plazi/market/web/security/SecurityConfig.java
@@ -6,22 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PlatziUserDetailsService service;
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(service);
     }
-    @Autowired
-    private JwtFilterRequest jwtFilterRequest;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests().antMatchers("/**/authenticate").permitAll()
